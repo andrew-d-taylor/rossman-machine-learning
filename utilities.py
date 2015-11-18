4f8219c2cf53aa0a0d8b2ba631b8730e86abe19f
Scott-K <scott.russell.kimball@gmail.com>
2015-11-18 14:34:31 -0500

    added storetype columns to dataframe. Transformed Assortment column to scalar values

--- utilities.py
+++ utilities.py
@@ -9,8 +9,12 @@ def createFlatCsv():
     trainDf = pd.read_csv('csv/train.csv')
     storeDf = pd.read_csv('csv/store.csv')
     combined = pd.merge(trainDf, storeDf, on='Store')
+    combined['StoreType_a'] = [1 if val == 'a' else 0 for val in combined['StoreType']]
+    combined['StoreType_b'] = [1 if val == 'b' else 0 for val in combined['StoreType']]
+    combined['StoreType_c'] = [1 if val == 'c' else 0 for val in combined['StoreType']]
+    combined['StoreType_d'] = [1 if val == 'd' else 0 for val in combined['StoreType']]
+    combined['Assortment'] = [1 if val == 'a' else 0 for val in combined['Assortment']]
     combined.to_csv('csv/combined.csv', sheet_name='train.csv X store.csv')
-
 
 def plot(estimate, response, responseLabel, predictor, predictorLabel):
     X_prime = np.linspace(predictor.min(), predictor.max(), 100)[:, np.newaxis]
